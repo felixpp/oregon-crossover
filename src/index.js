341c60808470eddffcfa73e4eba75771c3cb5578
Felix Perreault <perreault.felix@gmail.com>
2015-10-12 13:43:39 -0400

    Added getBranches method and tests

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@
 
 // Gets the Q library.
 var Q = require('q');
+var request = require('request');
 
-module.exports.configure = function () {
+module.exports.getBranches = function (account, apiVersion, includeChildren, includeDeleted) {
 
+    var url = "https://" + account +
+        ".visualstudio.com/defaultcollection/_apis/tfvc/branches?api-version=" + apiVersion +
+        "&includechildren=" + includeChildren + "&includedeleted=" + includeDeleted;
+
+    var defer = Q.defer();
+    request.get(url, function(err, res){
+        if(res.statusCode == '203'){
+            defer.resolve(res);
+        } else {
+            defer.reject(err.toString());
+        }
+
+    });
+
+    return defer.promise;
 };
