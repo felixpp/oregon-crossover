be0a1a7320fc1721f7f1a3f88666ca8692f038d7
oOKadOo <alexis.philippe@gmail.com>
2015-10-12 13:58:33 -0400

    we now have routes described by expressjs and green tests to prove it

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ var sys = require('util'),
 fs = require('fs'),
 http = require('http'),
 url = require('url');
+
 var express = require('express');
 var app = express();
 
@@ -10,9 +11,16 @@ app.get('/', function (req, res) {
   res.send('Hello World\n');
 });
 
+app.get('/authOnTFS', function(req,res){
+  res.send('I\'m now authenticated on TFS...mmm not for real.');
+});
+
 var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+
+
+
 });
